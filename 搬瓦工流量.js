afec55e25988b2cc76ebe8aac6a09d9bc8a5b225
smartmimi <43767309+smartmimi@users.noreply.github.com>
2021-02-18 14:08:58 +0800

    Update 搬瓦工流量.js
    change var to let
    delete blank row

--- 搬瓦工流量.js
+++ 搬瓦工流量.js
@@ -7,19 +7,18 @@ $http.request({
   method: "GET",
   url: url,
   handler: function (resp) {
-    var html = resp.data;
-
-    var datacounter = html["data_counter"];
-    var data_next_reset = html["data_next_reset"];
-    var data = (datacounter / (12024 * 1024 * 1024)).toFixed(2);
-    var reset = redate(data_next_reset);
+    let html = resp.data;
+    let datacounter = html["data_counter"];
+    let data_next_reset = html["data_next_reset"];
+    let data = (datacounter / (12024 * 1024 * 1024)).toFixed(2);
+    let reset = redate(data_next_reset);
     $ui.success("已用" + data + "/500G重置时间：" + reset);
   }
 });
 function redate(datein) {
-  var da = new Date(datein * 1000);
-  var year = da.getFullYear();
-  var month = da.getMonth() + 1;
-  var date = da.getDate();
+  let da = new Date(datein * 1000);
+  let year = da.getFullYear();
+  let month = da.getMonth() + 1;
+  let date = da.getDate();
   return [year, month, date].join("-");
 }
